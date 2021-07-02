43f268e7bac042ac4d4daee7c7372a4f3f28b20d
Rahul Passi <rahulpassi2412@gmail.com>
2021-07-03 00:19:18 +0530

    basic seacrh functionality added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import codes from '../../codes'
 import './index.scss'
+import { Input } from 'antd';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredCodes: codes
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = event => {
+    let filteredCodes = codes.filter(function (e) {
+      return e.code.includes(event.target.value);
+  });
+    this.setState({ filteredCodes:filteredCodes })
+  }
+
   render() {
     return (
       <div>
+        <Input onChange={this.handleChange} />
+
         {
-          codes.map(code => <li>{code.code}</li>)
+          this.state.filteredCodes.map(code => <li>{code.code}</li>)
         }
       </div>
     )
